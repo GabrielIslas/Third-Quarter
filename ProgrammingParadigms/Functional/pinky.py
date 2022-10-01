fe63be8ec0380a022252e70840c358d1cd2a7dd1
Gabriel Islas <60411092+Gaborro14@users.noreply.github.com>
2022-10-01 12:31:30 -0500

    pinky exercise progress

--- ProgrammingParadigms/Functional/pinky.py
+++ ProgrammingParadigms/Functional/pinky.py
@@ -1,55 +1,66 @@
+class Animal:
 
+    def __init__(self, iq, species, capabilities):
+        self.iq = iq
+        self.species = species
+        self.capabilities = capabilities
 
+    def __str__(self):
+        returnString = ""
+        returnString += f"IQ: {self.iq}\n"
+        returnString += f"Species: {self.species}\n"
+        returnString += f"Capabilities: {self.capabilities}\n"
+        return returnString
 
-def Animal(iq, species, capabilities):
-    return [iq, species, capabilities]
-
-def superiorIntelligence(animal, iq):
-    animal[0] += iq
-    return animal
+    def superiorIntelligence(self, addIQ):
+        self.iq += addIQ
         
-def pinkify(animal):
-    animal[2] = ""
-    return animal
+    def pinkify(self):
+        self.capabilities = []
 
-def superpowers(animal):
-    if animal[1] == "elephant":
-        animal[2].append("not to be afraid of mice")
-    elif animal[1] == "mouse" and animal[0] > 100:
-        animal[2].append("speak")
-    
-def anthropomorphic(animal):
-    if animal[0] > 60 and "speak" in animal[2]:
+    def superpowers(self):
+        if self.species == "elephant":
+            self.capabilities.append("not to be afraid of mice")
+        elif self.species == "mouse" and self.iq > 100:
+            self.capabilities.append("speak")
+        
+    def anthropomorphic(self):
+        if self.iq > 60 and "speak" in self.capabilities:
+            return True
+        return False
+
+    def notSoSane(self):
+        pinkCounter = 0
+        for ability in self.capabilities:
+            if self.pinkiesco(ability):
+                pinkCounter += 1
+        if pinkCounter > 2:
+            return True
+        return False
+        
+    def pinkiesco(ability):
+        abilityWords = ability.split(" ")
+        if len(abilityWords) != 2:
+            return False
+        if abilityWords[0] != "do":
+            return False
+        if len(abilityWords[1]) > 4:
+            return False
+        totalVowelCount = (abilityWords[1].count("a") + 
+                        abilityWords[1].count("e") + 
+                        abilityWords[1].count("i") + 
+                        abilityWords[1].count("o") + 
+                        abilityWords[1].count("u"))
+        if totalVowelCount != 1:
+            return False
         return True
-    return False
 
-def notSoSane(animal):
-    pinkCounter = 0
-    for ability in animal[2]:
-        if pinkiesco(ability):
-            pinkCounter += 1
-    if pinkCounter > 2:
-        return True
-    return False
-    
-def pinkiesco(ability):
-    abilityWords = ability.split(" ")
-    if len(abilityWords) != 2:
-        return False
-    if abilityWords[0] != "do":
-        return False
-    if len(abilityWords[1]) > 4:
-        return False
-    totalVowelCount = (abilityWords[1].count("a") + 
-                       abilityWords[1].count("e") + 
-                       abilityWords[1].count("i") + 
-                       abilityWords[1].count("o") + 
-                       abilityWords[1].count("u"))
-    if totalVowelCount != 1:
-        return False
-    return True
 
-def Experiment():
-    return []
-        
-        
+
+animal = Animal(75, "elephant", ["asdsad"])
+print(animal)
+animal.superpowers()
+print(animal)
+animal.superiorIntelligence(25)
+print(animal)
+print(animal.anthropomorphic())
