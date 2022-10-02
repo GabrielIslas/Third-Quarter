e799d2662c59aa333b12fc0336d9754588220eec
Gaborro14 <60411092+Gaborro14@users.noreply.github.com>
2022-10-02 02:12:07 -0500

    finished pinky exercise

--- ProgrammingParadigms/Functional/pinky.py
+++ ProgrammingParadigms/Functional/pinky.py
@@ -1,3 +1,5 @@
+from itertools import product
+
 class Animal:
 
     def __init__(self, iq, species, capabilities):
@@ -12,32 +14,32 @@ class Animal:
         returnString += f"Capabilities: {self.capabilities}\n"
         return returnString
 
-    def superiorIntelligence(self, addIQ):
-        self.iq += addIQ
+    def superiorIntelligence(animal, addIQ):
+        animal.iq += addIQ
         
-    def pinkify(self):
-        self.capabilities = []
+    def pinkify(animal):
+        animal.capabilities = []
 
-    def superpowers(self):
-        if self.species == "elephant":
-            self.capabilities.append("not to be afraid of mice")
-        elif self.species == "mouse" and self.iq > 100:
-            self.capabilities.append("speak")
+    def superpowers(animal):
+        if animal.species == "elephant":
+            animal.capabilities.append("not to be afraid of mice")
+        elif animal.species == "mouse" and animal.iq > 100:
+            animal.capabilities.append("speak")
         
-    def anthropomorphic(self):
-        if self.iq > 60 and "speak" in self.capabilities:
+    def anthropomorphic(animal):
+        if animal.iq > 60 and "speak" in animal.capabilities:
             return True
         return False
 
-    def notSoSane(self):
+    def notSoSane(animal):
         pinkCounter = 0
-        for ability in self.capabilities:
-            if self.pinkiesco(ability):
+        for ability in animal.capabilities:
+            if Animal.pinkiesco(ability):
                 pinkCounter += 1
         if pinkCounter > 2:
             return True
         return False
-        
+    
     def pinkiesco(ability):
         abilityWords = ability.split(" ")
         if len(abilityWords) != 2:
@@ -55,12 +57,97 @@ class Animal:
             return False
         return True
 
+    def generateWordsUpTo(length):
+        for x in range(1, length + 1):
+            Animal.generateWords(x)
+                
+    def generateWords(length):
+        vowels = ["a", "e", "i", "o", "u"]
+        consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n','p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+        if length > 4:
+            return
+        elif length == 1:
+            for vowel in vowels:
+                print(vowel)
+            return
+        elif length == 3 or length == 4:
+            consonants = list(product(consonants, repeat = length - 1))
+            for index in range(len(consonants)):
+                consonants[index] = "".join(consonants[index])
+        for index in range(length):
+            for vowel in vowels:
+                for consonant in consonants:
+                    print(consonant[:index] + vowel + consonant[index:])
+        
+class Experiment:
+    
+    def __init__(self, animal, transformations, criteria):
+        self.animal = animal
+        self.transformations = transformations
+        self.criteria = criteria
+        self.resultingAnimal = None
+        
+    def successfulExperiment(self):
+        animalCopy = self.animal
+        for transformation in self.transformations:
+            print(animalCopy)
+            transformation(animalCopy)
+        print(animalCopy)
+        self.resultingAnimal = animalCopy
+        return self.criteria(animalCopy)
+        
+class Report:
+    
+    def __init__(self, animals, capabilities, transformations, criteria):
+        self.animals = animals
+        self.capabilities = capabilities
+        self.transformations = transformations
+        self.criteria = criteria
+        self.experiments = []
+        
+    def GenerateInformation(self):
+        for animal in self.animals:
+            individualExperiment = Experiment(animal, self.transformations, self.criteria)
+            individualExperiment.successfulExperiment()
+            self.experiments.append(individualExperiment)
+            
+    def IQReport(self):
+        self.GenerateInformation()
+        for experiment in self.experiments:
+            for capability in self.capabilities:
+                if capability in experiment.resultingAnimal.capabilities:
+                    print(f"Animal: {experiment.resultingAnimal.species}; IQ: {experiment.resultingAnimal.iq}")
+                    break
+                
+    def speciesReport(self):
+        self.GenerateInformation()
+        for experiment in self.experiments:
+            capabilityCounter = 0
+            for capability in self.capabilities:
+                if capability not in experiment.resultingAnimal.capabilities:
+                    break
+                else:
+                    capabilityCounter += 1
+            if capabilityCounter == len(self.capabilities):
+                print(f"Animal: {experiment.resultingAnimal.species}")
+    
+    def capacitiesReport(self):
+        self.GenerateInformation()
+        for experiment in self.experiments:
+            hasCapability = False
+            for capability in self.capabilities:
+                if capability in experiment.resultingAnimal.capabilities:
+                    hasCapability = True
+                    break
+            if not hasCapability:
+                print(f"Animal: {experiment.resultingAnimal.species}; No. Capacities: {len(experiment.resultingAnimal.capabilities)}")
 
+animalTest = Animal(17, "mouse", ["destroy the world", "make soulless plans"])
 
-animal = Animal(75, "elephant", ["asdsad"])
-print(animal)
-animal.superpowers()
-print(animal)
-animal.superiorIntelligence(25)
-print(animal)
-print(animal.anthropomorphic())
+print(animalTest)
+
+addSpecificIQ = lambda animalIQ: Animal.superiorIntelligence(animalIQ, 100)
+experiment = Experiment(animalTest, [Animal.pinkify, addSpecificIQ, Animal.superpowers], Animal.anthropomorphic)
+print(experiment.successfulExperiment())
+
+Animal.generateWords(4)
