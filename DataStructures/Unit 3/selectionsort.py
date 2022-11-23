b979761e3ee2b6ee6b74f941fe13a93ca5d7e83a
Gabriel Islas <60411092+Gaborro14@users.noreply.github.com>
2022-11-23 08:19:03 -0600

    a bunch of things

--- DataStructures/Unit 3/selectionsort.py
+++ DataStructures/Unit 3/selectionsort.py
@@ -3,10 +3,13 @@ def selection_sort(list):
     while sorted != len(list):
         smallest = list[sorted]
         smallestIndex = sorted
+        found = False
         for i in range(sorted, len(list)):
             if list[i] < smallest:
                 smallest = list[i]
                 smallestIndex = i
+        print(f"Selected smallest number is {smallest} in index {smallestIndex} and it should be in {sorted}.")
+        print(list)
         list[smallestIndex] = list[sorted]
         list[sorted] = smallest
         sorted += 1
@@ -14,4 +17,6 @@ def selection_sort(list):
 
 list1 = [5, 4, 3, 2, 1]
 list2 = [6, 8, 4, 10, 70, 52, 31]
-print(selection_sort(list2))
+list3 = [20, 30, 40, 90, 50, 60, 70, 80, 100, 110]
+selection_sort(list3)
+
